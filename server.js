6b9a39b08f9df4026be094595329ebb3a6bd0f02
Oge Obubu <ogeobubu@gmail.com>
2021-03-10 07:06:18 +0100

    resolved production path

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
